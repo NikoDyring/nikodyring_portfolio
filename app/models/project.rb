40a69be2386fdab093543cf32e2b327f63eb35a6
Nikolaj Dyring <nikodyring@gmail.com>
2025-10-30 21:38:45 +0100

    Mess with Kamal

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ApplicationRecord
   has_one_attached :icon
-  enum status: { unpublished: 0, published: 1 }
-  enum category: { software: 0, game: 1, web: 2, other: 3 }
-  enum programming_language: { ruby_on_rails: 0, c_sharp: 1, javascript: 2, python: 3, java: 4 }
+  enum :status, { unpublished: 0, published: 1 }
+  enum :category, { software: 0, game: 1, web: 2, other: 3 }
+  enum :programming_language, { ruby_on_rails: 0, c_sharp: 1, javascript: 2, python: 3, java: 4 }
 
   validates :title, presence: true
   validates :description, presence: true
