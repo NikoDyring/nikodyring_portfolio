626a8cad5c0cb5b03b15eb85b45347326b8c9f36
Nikolaj Dyring <nikodyring@gmail.com>
2025-11-15 03:28:27 +0100

    Revert route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  # Keep your global root
   root to: redirect("/#{I18n.default_locale}/articles")
 
   scope "/:locale" do
     get "/about", to: "about#index", as: :about
-    resources :articles, controller: "articles"
-    resources :projects, controller: "projects"
+    resources :articles
+    resources :projects
     devise_for :users
   end
 
-  get "up", to: "rails/health#show", as: :rails_health_check
+  # Health Check
+  get "up" => "rails/health#show", as: :rails_health_check
 end
